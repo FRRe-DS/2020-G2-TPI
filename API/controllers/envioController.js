d6f6b59b73c84c797aaf55566e249f3747a4a068
Mariano Troncoso <petotronco@gmail.com>
2020-06-09 12:15:23 -0300

    planteo de actualización de peticiones, campo de respondidaCompletamente

--- API/controllers/envioController.js
+++ API/controllers/envioController.js
@@ -6,13 +6,26 @@ const Peticion = require('../models/Peticion');
 exports.nuevoEnvio = async(req,res,next) =>{
     const envio = new Envio(req.body);
 
+    const envioActual = req.body.Envio
+
     try{
-        // verificar petición existe
-        if(req.body.Envio.hasOwnProperty("idPeticion")){
+        // envio tiene id peticion? 
+        if(envioActual.hasOwnProperty("idPeticion")){
             console.log('EN LA PETICIÓN EXISTE EN EL ENVIO')
+            
             // buscar peticion en base de datos
+            const peticionActual = Peticion.findById(envioActual.idPeticion)
 
             // si la encontramos, update 
+            if(peticionActual){
+                // actualizar la peticion 
+
+                // cambio de estado de peticion? 
+
+            } else{
+                // error, no existe la peticion en la base de datos
+                res.json({mensaje: 'No existe la petición'})
+            }
         } else {
             console.log('EN LA PETICIÓN NO EXISTE EN EL ENVIO')
             // guardar envio
