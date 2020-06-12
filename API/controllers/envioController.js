43c36d0e9d530b0c8928490b827d400a9b7491b6
Mariano Troncoso <petotronco@gmail.com>
2020-06-12 10:55:08 -0300

    actualización de los valores de la petición al hacer un envio

--- API/controllers/envioController.js
+++ API/controllers/envioController.js
@@ -2,7 +2,6 @@ const Envio = require('../models/Envios');
 const Peticion = require('../models/Peticion');
 
 //crear un nuevo Envio
-
 exports.nuevoEnvio = async(req,res,next) =>{
     const envio = new Envio(req.body);
 
@@ -13,22 +12,59 @@ exports.nuevoEnvio = async(req,res,next) =>{
     try{
         // envio tiene id peticion? 
         if(envioActual.hasOwnProperty("idPeticion")){
-            console.log('EN LA PETICIÓN EXISTE EN EL ENVIO')
+            console.log('LA PETICIÓN EXISTE EN EL ENVIO')
+
+            // FALTA TRATAR MEDICOS!!!!!!
+            const recursos = ["camillas","alcoholLitros","jabonLitros","barbijos","jeringas","cofias"]
             
             // buscar peticion en base de datos
-            const peticionActual = Peticion.findById(envioActual.idPeticion)
+            Peticion.findById(envioActual.idPeticion, (error, peti) => {
+
+                if(peti){
+                    console.log('EXISTE LA PETICION!')
+                    console.log('LA PETICIÓN ANTES DE REALIZAR EL ENVIO:')
+                    console.log(peti.Peticion)
+
+                    // ACTUALIZAR LA PETICION
+                    // actualizar valores
+                    for(var key in peti.Peticion){
+                        if(peti.Peticion.hasOwnProperty(key)){
+
+                            if(recursos.includes(key) && envioActual[key] != undefined){
+                                console.log('Se han enviado: '+ envioActual[key] + 'del recurso:' + key)
+                                peti.Peticion[key] -= envioActual[key]
+                                console.log('Falta enviar: ' + peti.Peticion[key] + 'del recurso ' + key)
+                            }
+                                 
+                        }
+                    }
+
+                    console.log('LA PETICIÓN DESPUES DE REALIZAR EL ENVIO:')
+                    console.log(peti.Peticion)
+
+                    // actualizar en base de datos
+                   Peticion.findByIdAndUpdate(envioActual.idPeticion, {"Peticion": peti.Peticion}, {useFindAndModify: false} ,(err, result) => {
+                       if(err){
+                           res.send(err)
+                       } else{
+                           res.json({mensaje:"Envio de prueba realizado"});
+                       }
+                   })
+                    
+                    // cambio de estado de peticion? 
+                   // we pera
+                    
+    
+                } else{
+                    // error, no existe la peticion en la base de datos
+                    res.json({mensaje: error})
+                }
+            })
 
             // si la encontramos, update 
-            if(peticionActual){
-                // actualizar la peticion 
-                // cambio de estado de peticion? 
-
-            } else{
-                // error, no existe la peticion en la base de datos
-                res.json({mensaje: 'No existe la petición'})
-            }
+            
         } else {
-            console.log('EN LA PETICIÓN NO EXISTE EN EL ENVIO')
+            console.log('LA PETICIÓN NO EXISTE EN EL ENVIO')
             // guardar envio
             // guardar envio
             await envio.save();
