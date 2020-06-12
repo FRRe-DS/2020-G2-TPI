c74ce67eff96322ba2008382a76bbb61ca6df0fb
FacuCuzziol <facucuzziol@gmail.com>
2020-06-12 18:22:27 -0300

    adding medicos and recursos data into mongo

---
+++ API/controllers/recursosController.js
@@ -0,0 +1,38 @@
+const Recursos = require('../models/Recursos');
+
+
+//SOLO AL PRINCIPIO, TOCAS DE VUELTA ESTE METODO Y ROMPES TODO
+//TENEMOS QUE USAR UN METODO PARA ACTUALIZAR EL UNICO ELEMENTO DE LA COLECCION
+
+
+exports.registrarRecursos = async (req,res,next) =>{
+    
+    const recursos = new Recursos(req.body);
+    console.log(req.body);
+    try {
+        await recursos.save();
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.json({mensaje:"Los recursos se agregaron correctamente"});
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
+
+//cuando obtengo las Recursos
+
+
+exports.getRecursos = async(req,res,next) =>{
+    try {
+        const recursos = await Recursos.find({});
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.json(recursos);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
