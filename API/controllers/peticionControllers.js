cace6dad1a6dc4bf639d05594b323b72f2b6025b
Mariano Troncoso <petotronco@gmail.com>
2020-06-29 18:18:27 -0300

    validacion de idCentro en capa de servicio

--- API/controllers/peticionControllers.js
+++ API/controllers/peticionControllers.js
@@ -1,40 +1,32 @@
 const Peticion = require('../models/Peticion');
-const CentroHospitalario = require('../models/CentrosHospitalarios');
+const Validacion = require('../validacion/peticionValidator')
 
-
-//cuando creo una nueva peticionaa
-
-exports.nuevaPeticion = async (req,res,next) =>{
-
-    // validacion del centro hospitalario 
-    // PODRIA IR EN VALIDACIONES
-    // console.log(req.body.Peticion.idCentro)
+exports.nuevaPeticion = (req,res,next) =>{
 
     // IMPORTANTE: asumimos que NO HAY IDs repetidos!!!!
-    const centro = await CentroHospitalario.find({'idCentro': req.body.Peticion.idCentro})
-    if(centro.length == 1){
-        // console.log('ID VALIDO!')
-    
-        // campo agregado para saber cuando esta completamente respondida
-        req.body.Peticion.respondidaCompletamente = false;
-        req.body.Peticion.rechazada = false;
 
-        const peticion = new Peticion(req.body);
+    Validacion.existeCentroHospitalario(req.body.Peticion.idCentro).then( async (existe) => {
+        
+        if(existe){
+            req.body.Peticion.respondidaCompletamente = false;
+            req.body.Peticion.rechazada = false;
 
-        try {
-            await peticion.save();
-            res.statusCode = 200;
-            res.setHeader('content-type', 'application/json');
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.json({mensaje:"La petición se agregó correctamente"});
-        } catch (error) {
-                console.log(error);
-                next();
-            }
+            const peticion = new Peticion(req.body);
+
+            try {
+                await peticion.save();
+                res.statusCode = 200;
+                res.setHeader('content-type', 'application/json');
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.json({mensaje:"La petición se agregó correctamente"});
+            } catch (error) {
+                    console.log(error);
+                    next();
+                }
         }else{
-            // console.log('ID NOOO VALIDO!')
             res.json({mensaje:"idCentro NO válido"});
         }
+    })
 }
 
 // cuando obtengo las peticiones 
