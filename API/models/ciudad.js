042e65e7de41afca78f665fc7130c3a2f54a535f
AnastaciusWright <alexandronadal@gmail.com>
2020-06-29 16:09:21 -0300

    Modelo Ciudades@

---
+++ API/models/ciudad.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Ciudad = new mongoose.Schema({
+                idCiudad:{
+                    type: Number,
+                    required: true
+                },
+                nombreCiudad:{
+                    type: String,
+                    required: true
+                }
+
+            })
