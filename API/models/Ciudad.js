8c5a801d00cc62dc5fd28ee662f66df04be83d26
Nicolas Zini <nicolaszini8@gmail.com>
2020-06-29 19:11:26 -0300

    update models ciudad

--- API/models/Ciudad.js
+++ API/models/Ciudad.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
 
 const Ciudad = new mongoose.Schema({
-                idCiudad:{
-                    type: Number,
-                    required: true
-                },
-                nombreCiudad:{
-                    type: String,
-                    required: true
-                },
-                latitud:{
-                    type: Number,
-                    required: true
-                },
-                longitud:{
-                    type: Number,
-                    required: true
-                }
+    
+    Ciudades:[{
+        idCiudad:{
+            type: Number,
+            required: true
+        },
+        nombreCiudad:{
+            type: String,
+            required: true
+        },
+        latitud:{
+            type: Number,
+            required: true
+        },
+        longitud:{
+            type: Number,
+            required: true
+        }
+    }]
+                
 
-            });
+});
 
 module.exports = mongoose.model('Ciudad',Ciudad)
