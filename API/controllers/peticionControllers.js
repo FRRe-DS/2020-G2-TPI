90454fe0b98c8e878313710ef929b30c81972738
Mariano Troncoso <petotronco@gmail.com>
2020-06-29 12:43:29 -0300

    control de idCentro al hacer una peticion

--- API/controllers/peticionControllers.js
+++ API/controllers/peticionControllers.js
@@ -1,31 +1,43 @@
 const Peticion = require('../models/Peticion');
+const CentroHospitalario = require('../models/CentrosHospitalarios');
 
 
-//cuando creo una nueva peticion
+//cuando creo una nueva peticionaa
 
 exports.nuevaPeticion = async (req,res,next) =>{
+
+    // validacion del centro hospitalario 
+    // PODRIA IR EN VALIDACIONES
+    // console.log(req.body.Peticion.idCentro)
+
+    // IMPORTANTE: asumimos que NO HAY IDs repetidos!!!!
+    const centro = await CentroHospitalario.find({'idCentro': req.body.Peticion.idCentro})
+    if(centro.length == 1){
+        // console.log('ID VALIDO!')
     
-    // campo agregado para saber cuando esta completamente respondida
-    req.body.Peticion.respondidaCompletamente = false;
-    req.body.Peticion.rechazada = false;
+        // campo agregado para saber cuando esta completamente respondida
+        req.body.Peticion.respondidaCompletamente = false;
+        req.body.Peticion.rechazada = false;
 
-    const peticion = new Peticion(req.body);
+        const peticion = new Peticion(req.body);
 
-    try {
-        await peticion.save();
-        res.statusCode = 200;
-        res.setHeader('content-type', 'application/json');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json({mensaje:"La petición se agregó correctamente"});
-    } catch (error) {
-        console.log(error);
-        next();
-    }
+        try {
+            await peticion.save();
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({mensaje:"La petición se agregó correctamente"});
+        } catch (error) {
+                console.log(error);
+                next();
+            }
+        }else{
+            // console.log('ID NOOO VALIDO!')
+            res.json({mensaje:"idCentro NO válido"});
+        }
 }
 
-//cuando obtengo las peticiones 
-
-
+// cuando obtengo las peticiones 
 exports.obtenerPeticiones = async(req,res,next) =>{
     try {
         const peticiones = await Peticion.find({});
