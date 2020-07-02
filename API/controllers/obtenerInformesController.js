5dacfab3b5a47723e42d71ac683717306a40530c
Mariano Troncoso <petotronco@gmail.com>
2020-07-02 20:57:04 -0300

    COMMIT CON ERROR, nuevo intento de simular obtener informes pero ahora con un endpoint de peticiones

--- API/controllers/obtenerInformesController.js
+++ API/controllers/obtenerInformesController.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const InformeHospitalMinisterio = require('../models/InformeHospitalAMinisterio');
 var request = require('request');
 const ciudades = require('../models/Ciudad');
+const Peticion = require('../models/Peticion');
 
 exports.registrarInformes = async (req,res,next) => {
 
   var options = {
     'method': 'GET',
-    'url': 'http://localhost:5000/informes',
+    'url': 'https://6iubewzdng.execute-api.sa-east-1.amazonaws.com/dev/peticiones',
     'headers': {
+      'x-api-key': 'FTlS2bc9lo1OtmzHCBrju4ZL8PqFM5yr4JB775RR'
     }
   };
 
@@ -28,38 +30,18 @@ exports.registrarInformes = async (req,res,next) => {
     for( item in respuesta.Informes){
 
       console.log(respuesta.Informes[item])
-      
-      if(respuesta.Informes[item].pruebasRealizadas.realizadas == (respuesta.Informes[item].pruebasRealizadas.sinResultado + respuesta.Informes[item].pruebasRealizadas.positivas +respuesta.Informes[item].pruebasRealizadas.negativas)){
-
-        const nuevoInforme = new InformeHospitalMinisterio(respuesta.Informes[item]);
-        const ciudad = (await ciudades.find().limit(1))[0].Ciudades;
-        var ciudadValida = false;
-        
-        // control de ciudad valida
-        ciudad.forEach(element => {
-            if(element.idCiudad==nuevoInforme.idCiudad){
-                ciudadValida = true;
-            }
-        });
 
         try {
-            if(ciudadValida){   
-                await nuevoInforme.save(); 
-                res.statusCode = 200;
-                res.setHeader('content-type', 'application/json');
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.json({mensaje:"El informe se guardo en la base"});
-            } else{
-                res.json({mensaje:"La ciudad NO es valida"});
-            }     
+          let peticionTemporal = new Peticion(respuesta.Informes[item])
+          await peticionTemporal.save(); 
+          res.statusCode = 200;
+          res.setHeader('content-type', 'application/json');
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.json({mensaje:"El informe se guardo en la base"});    
         } catch (error) {
             console.log(error);
             next();
         }
-    } else{
-        res.json({mensaje:"Errores en las cantidades de tests realizados del informe"});
-    }
-
     
     }
 
