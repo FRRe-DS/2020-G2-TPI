006c0e6d1b5f49e5436c4ec03e991ae741fcc193
AnastaciusWright <alexandronadal@gmail.com>
2020-07-04 17:20:58 -0300

    Now it deploys

--- API/index.js
+++ API/index.js
@@ -4,37 +4,22 @@ const mongoose = require('mongoose');
 const PORT = 5000;
 const {MONGOURI} = require('./keys');
 const sls = require('serverless-http');
-const session = require('express-session');
 
 
-
-require('./models/Ministerio');
-require('./models/Peticion');
-require('./models/CentrosHospitalarios');
-require('./models/InformeHospitalAMinisterio');
-require('./models/Recursos');
-require('./models/Medicos');
-
-
-
-
-
-
-let db;
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:false
+    useUnifiedTopology:true
 })
 .catch(err=>{
     console.log('Error de conexion',err);
 })
 .then(()=> {
     console.log("Me conecte a mongo")
-    app.use(require('./routes/index')); 
-
+    
 })
-
+console.log(mongoose.connection)
+app.use(require('./routes/index')); 
 
 app.listen(PORT,()=>{
     console.log("Servidor corriendo en el puerto",PORT);
