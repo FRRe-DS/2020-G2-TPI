266f180f64cfd5f57e55d0e4973fdf34f6cde64e
AnastaciusWright <alexandronadal@gmail.com>
2020-07-04 16:33:08 -0300

    Sesion removida

--- API/index.js
+++ API/index.js
@@ -5,20 +5,18 @@ const PORT = 5000;
 const {MONGOURI} = require('./keys');
 const sls = require('serverless-http');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 
-const bodyParser = require('body-parser')
-const {EXPRESS_SECRET} = require('./secrets');
+
+
 require('./models/Ministerio');
 require('./models/Peticion');
 require('./models/CentrosHospitalarios');
 require('./models/InformeHospitalAMinisterio');
 require('./models/Recursos');
 require('./models/Medicos');
-app.use(express.json());
 
-app.use(bodyParser.json());      
-app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 
 
@@ -26,29 +24,16 @@ let db;
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:false
 })
 .catch(err=>{
     console.log('Error de conexion',err);
 })
 .then(()=> {
     console.log("Me conecte a mongo")
-    //console.log(mongoose.connection);
-    var mongoStore = new MongoStore({
-        mongooseConnection:mongoose.connection
-    });
-    app.use(
-        session({
-            secret:EXPRESS_SECRET,
-            clear_interval: 900,
-            cookie:{ maxAge: 86400},
-            store: mongoStore
-        })
-    );
     app.use(require('./routes/index')); 
 
-}
-    )
+})
 
 
 app.listen(PORT,()=>{
